aff139a4c95832a15ce874fb37a131a46830d72b
ariful97768 <ariful97768@gmail.com>
2025-07-20 15:37:18 +0600

    error recovery using reset for client component and startTransition with router.refresh() for server component.

--- app/(main)/product/[productId]/reviews/[reviewId]/error.tsx
+++ app/(main)/product/[productId]/reviews/[reviewId]/error.tsx
@@ -1,6 +1,22 @@
 "use client";
-const error = ({ error }: { error: Error }) => {
-  return <h2>{error.message}</h2>;
+
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
+  const router = useRouter();
+  const recover = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+  return (
+    <div>
+      <h2>{error.message}</h2>
+      <button onClick={recover}>retry</button>
+    </div>
+  );
 };
 
-export default error;
+export default Error;
