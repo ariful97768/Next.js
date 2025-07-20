53467da88e9e78aab4ba1f44c00686ac28922178
ariful97768 <ariful97768@gmail.com>
2025-07-20 15:24:28 +0600

    added routes links for easy navigation.

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,13 +1,19 @@
-
+import Link from "next/link";
 
 export default async function Home() {
-
   return (
     <>
-      
       <main>
-home
-      </main>
+        home
+        <li><Link href={'/login'}>Login</Link></li>
+        <li><Link href={'/register'}>register</Link></li>
+        <li><Link href={'/counter'}>counter</Link></li>
+        <li><Link href={'/docs/something'}>docs</Link></li>
+        <li><Link href={'/product'}>product</Link></li>
+        <li><Link href={'/product/1'}>product/1</Link></li>
+        <li><Link href={'/product/1/reviews/10'}>reviews</Link></li>
+        <li><Link href={'/order-product'}>order</Link></li>
+        </main>
     </>
   );
 }
