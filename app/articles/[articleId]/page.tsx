c39969dd953231f75550807516a182166aee071f
ariful97768 <ariful97768@gmail.com>
2025-07-20 12:51:58 +0600

    Params and searchParams

---
+++ app/articles/[articleId]/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+
+const ArticleDetails = async ({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ articleId: string }>;
+  searchParams: Promise<{ lang?: "en" | "bn" | "sp" }>;
+}) => {
+  const { articleId } = await params;
+  const { lang = "en" } = await searchParams;
+  return (
+    <div>
+      <h1>News article {articleId}</h1>
+      <p>Reading in language {lang}</p>
+      <div>
+        <Link href={`/articles/${articleId}?lang=en`}>English</Link>
+        <Link href={`/articles/${articleId}?lang=bn`}>Bangla</Link>
+        <Link href={`/articles/${articleId}?lang=sp`}>Spanish</Link>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleDetails;
