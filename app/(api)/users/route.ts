a0b8403054662a92645b7ba3d17e58c195cac834
ariful97768 <ariful97768@gmail.com>
2025-07-22 14:32:16 +0600

    Headers and Cookies

--- app/(api)/users/route.ts
+++ app/(api)/users/route.ts
@@ -1,10 +1,23 @@
 import { type NextRequest } from 'next/server';
 import { comments } from '../data';
+// import { headers, cookies } from 'next/headers';
 
-export function GET(req: NextRequest) {
+export async function GET(req: NextRequest) {
+    // const requestHeaders = new Headers(req.headers).get('HeadersName')
+    // const headersList = (await headers()).get('HeadersName')
+
+    // const theme = req.cookies.get('theme')
+    // const cookieStore = (await cookies()).get('CookieName')
+
     const query = req.nextUrl.searchParams.get('query')
     const filteredComments = query ? comments.filter(comment => comment.text.includes(query)) : comments
-    return Response.json(filteredComments)
+
+    return Response.json(filteredComments, {
+        headers: {
+            'Content-Type': /** 'application/json'*/ 'text/html',
+            'Set-Cookie': 'theme=dark'
+        }
+    })
 };
 
 export async function POST(request: Request) {
