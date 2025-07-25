aca2fe2b00bf1ae0fbdebf00da7c0a409c8e67a6
ariful97768 <ariful97768@gmail.com>
2025-07-26 00:16:51 +0600

    Data fetching in client component

---
+++ app/users-client/page.tsx
@@ -0,0 +1,45 @@
+'use client'
+import React, { useEffect, useState } from "react";
+
+type Users = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+};
+
+const UsersClient = () => {
+  const [users, setUsers] = useState<Users[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        if (!res.ok) throw new Error("Failed to fetch users");
+        const data = await res.json();
+        setUsers(data);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("An unknown error occurred");
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchUsers();
+  }, []);
+  if (error) return <div>{error}</div>;
+  if (loading) return <div>Loading...</div>;
+  return users.map((user) => (
+    <div key={user.id}>
+      <div>Username: {user.username}</div>
+      <div>Email: {user.email}</div>
+    </div>
+  ));
+};
+
+export default UsersClient;
